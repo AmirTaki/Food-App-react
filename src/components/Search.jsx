fd2e77e0ec04600495b125f70583144aede5d135
amir <amirtaki623@gmail.com>
2025-06-10 03:15:27 +0330

    Making API Call In React

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react"
 
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "7b3839cd485e4a858fdaeb01f35bf91f";
+
 export default function Search (){
     const [query, setQuery] = useState("");
     // Syntax of the useEffecct hook :  useEffect(()=> {}, [])
     useEffect(()=> {
-        function demo (){
-            console.log("Demo function executed");
+        async function fetchFood(){
+            const responsive = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+            const data = await responsive.json()
+            console.log(data.results)
         }
-        demo()
+        fetchFood()
+    
     }, [query])
 
     return(
