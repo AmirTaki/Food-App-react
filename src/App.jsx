7ff77c97745996a8479c9e3f6791b6fba4878904
amir <amirtaki623@gmail.com>
2025-06-11 19:28:25 +0330

    Outer Container Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Search from "./components/Search"
 import FoodList from "./components/FoodList"
 import Nav from "./components/Nav"
 import './App.css'
+import Container from "./components/Container"
 function App() {
   const [foodData, setFoodData] = useState([])
   return (
     <>
       <Nav />
       <Search foodData = {foodData}   setFoodData = {setFoodData}/>
-      <FoodList foodData = {foodData} />
-      
+      <Container>
+        <FoodList foodData = {foodData} />
+      </Container>
     </>
   )
 }
--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -2,10 +2,10 @@ import FoodItem from "./FoodItem";
 
 export default function FoodList ({foodData}) {
     return(
-        <>
+        <div>
         {foodData.map((food)=>(
             <FoodItem key = {food.id} food = {food}/>
       )) }
-        </>
+        </div>
     )
 }
